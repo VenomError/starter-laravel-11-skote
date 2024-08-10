62e51555951870a5ff265262e1bacabb5e33d46c
VenomError <venoms00001@gmail.com>
2024-08-10 18:16:41 +0800

    Flasher

---
+++ config/flasher.php
@@ -0,0 +1,66 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace Flasher\Laravel\Resources;
+
+return [
+    // Default notification library (e.g., 'flasher', 'toastr', 'noty', etc.)
+    'default' => 'flasher',
+
+    // Path to the main JavaScript file of PHPFlasher
+    'main_script' => '/vendor/flasher/flasher.min.js',
+
+    // Path to the stylesheets for PHPFlasher notifications
+    'styles' => [
+        '/vendor/flasher/flasher.min.css',
+    ],
+
+    // Whether to translate PHPFlasher messages using Laravel's translation service
+    'translate' => true,
+
+    // Automatically inject PHPFlasher assets into HTML response
+    'inject_assets' => true,
+
+    // Configuration for the flash bag (converting Laravel flash messages)
+    // Map Laravel session keys to PHPFlasher types
+    'flash_bag' => [
+        'success' => ['success'],
+        'error' => ['error', 'danger'],
+        'warning' => ['warning', 'alarm'],
+        'info' => ['info', 'notice', 'alert'],
+    ],
+
+    // Filter criteria for notifications (e.g., limit number, types)
+    'filter' => [
+        'limit' => 5, // Limit the number of displayed notifications
+    ],
+
+    'plugins' => [
+        'sweetalert' => [
+            'scripts' => [
+                '/vendor/flasher/sweetalert2.min.js',
+                '/vendor/flasher/flasher-sweetalert.min.js',
+            ],
+            'styles' => [
+                '/vendor/flasher/sweetalert2.min.css',
+            ],
+            'options' => [
+                // Optional: Add global options here
+                'position' => 'center',
+            ],
+        ],
+        'notyf' => [
+            'scripts' => [
+                '/vendor/flasher/flasher-notyf.min.js',
+            ],
+            'styles' => [
+                '/vendor/flasher/flasher-notyf.min.css',
+            ],
+            'options' => [
+                // Optional: Add global options here
+                'dismissible' => true,
+            ],
+        ],
+    ],
+];
